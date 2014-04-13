2c51a1c87ea1545a49015c78ec8ae02f08dfb876
Craig Burke <craig@craigburke.com>
2014-04-13 13:54:10 -0400

    Change Journal id to String

--- src/main/groovy/com/craigburke/doogie/dbo/Journal.groovy
+++ src/main/groovy/com/craigburke/doogie/dbo/Journal.groovy
@@ -3,13 +3,11 @@ package com.craigburke.doogie.dbo
 import org.mongodb.morphia.annotations.Entity
 import org.mongodb.morphia.annotations.Id
 
-import org.bson.types.ObjectId
-
 @Entity(value = "journal", noClassnameStored = true)
 class Journal {
 
     @Id
-    ObjectId id
+    String id
 
     String title
     Date date
