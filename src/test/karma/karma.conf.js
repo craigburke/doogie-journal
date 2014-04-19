5a1a2709dc0dcf68781ea932a925c21238e7adad
Craig Burke <craig@craigburke.com>
2014-04-19 07:21:13 -0400

    Cleanup for CI

--- src/test/karma/karma.conf.js
+++ src/test/karma/karma.conf.js
@@ -1,19 +1,8 @@
-// Karma configuration
-// Generated on Wed Apr 02 2014 08:50:54 GMT-0400 (EDT)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../../',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
-    // list of files / patterns to load in the browser
     files: [
         'ratpack/public/vendor/angularjs/angular.min.js',
         'ratpack/public/vendor/angularjs/angular-route.min.js',
@@ -27,50 +16,14 @@ module.exports = function(config) {
         'test/karma/unit/*.js'
     ],
 
-
-    // list of files to exclude
-    exclude: [
-      
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    exclude: [    ],
+    preprocessors: { },
     reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
 };
