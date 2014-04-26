9c50b939b503df08a1f7faf6f0dbbb0125e2624d
Craig Burke <craig@craigburke.com>
2014-04-26 15:06:44 -0400

    Fixing deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ DOOGIE_HOME="/web/doogie"
 DOOGIE_TEMP="/tmp/doogie"
 
 rm -rf $DOOGIE_HOME/*
-cp -R $DOOGIE_TEMP/install/doogie $DOOGIE_HOME
+cp -R $DOOGIE_TEMP/build/install/doogie/* $DOOGIE_HOME
 rm -rf $DOOGIE_TEMP
 
