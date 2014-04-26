ad63d46629bb17fba5d88b0faafe849fa93ca08b
Craig Burke <craig@craigburke.com>
2014-04-26 12:01:41 -0400

    Adding build/test script for CI

---
+++ scripts/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+function set_twitter_config {
+	cat <<-EOF >> src/ratpack/twitter4j.properties
+	oauth.consumerKey=$TWITTER_CONSUMER_KEY
+	oauth.consumerSecret=$TWITTER_CONSUMER_SECRET
+	oauth.accessToken=$TWITTER_ACCESS_TOKEN
+	oauth.accessTokenSecret=$TWITTER_ACCESS_TOKEN_SECRET
+	EOF
+}
+
+function start_selenium {
+	sudo start xvfb
+	wget http://selenium.googlecode.com/files/selenium-server-standalone-2.35.0.jar --quiet
+	java -jar selenium-server-standalone-2.35.0.jar > /dev/null 2>&1 &
+}
+
+function npm_install {
+	for PLUGIN in $1
+	do
+		sudo npm install -g $PLUGIN --silent
+	done
+
+	sudo npm install --silent
+}
+
+function run_karma_tests {
+	karma start src/test/js/karma.conf.js --single-run
+}
+
+function run_spock_tests {
+	./gradlew test
+}
+
+function run_protractor_tests {
+	./gradlew run  > /dev/null 2>&1 &
+	while ! curl http://localhost:5050/ &> /dev/null; do sleep 5; done
+	protractor src/test/js/protractor.conf.js
+}
+
+function build_app {
+	./gradlew installApp
+}
+
+set_twitter_config
+start_selenium
+npm_install "karma karma-cli protractor"
+run_karma_tests
+run_spock_tests
+run_protractor_tests
+build_app
